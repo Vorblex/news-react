7ca4620fb360cbf0d0066f731514733e255d5068
Vorblex <vorblex@gmail.com>
2024-07-26 23:45:36 +0100

    categories

---
+++ src/constants/index.js
@@ -0,0 +1,16 @@
+export const CATEGORIES_LIST = [
+  'regional',
+  'technology',
+  'lifestyle',
+  'business',
+  'general',
+  'science',
+  'world',
+  'health',
+  'food',
+  'game',
+  'education',
+  'music',
+]
+
+export const DEFAULT_CATEGORY = 'all'
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -7,42 +7,63 @@ import api from '@/api'
 import NewsList from '@/components/NewsList/NewsList'
 import Skeleton from '@/components/Skeleton/Skeleton'
 import Pagination from '@/components/Pagination/Pagination'
+import { CATEGORIES_LIST, DEFAULT_CATEGORY } from '@/constants'
+import Categories from '@/components/Categories/Categories'
 
 function Main() {
   const [news, setNews] = useState([])
   const [categories, setCategories] = useState([])
+  const [selectedCategory, setSelectedCategory] = useState(DEFAULT_CATEGORY)
   const [loading, setLoading] = useState(true)
   const [currentPage, setCurrentPage] = useState(1)
   const pageSize = 10
   const totalPages = 10
 
   async function getCategories() {
-    const { data } = await api.news.getCategories()
-    setCategories(data.categories)
+    // const { data } = await api.news.getCategories()
+    // setCategories(data.categories)
+    setCategories([DEFAULT_CATEGORY, ...CATEGORIES_LIST])
   }
 
   async function getNews() {
     setLoading(true)
-    const { data } = await api.news.get(currentPage)
+    const { data } = await api.news.get(currentPage, selectedCategory)
     setNews(data.news)
     setLoading(false)
   }
 
   useEffect(() => {
     getNews()
-  }, [currentPage])
+  }, [currentPage, selectedCategory])
 
-  // useEffect(() => {
-  //   getCategories()
-  // }, [])
+  useEffect(() => {
+    getCategories()
+  }, [])
 
   return (
-    <main>
-      <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={setCurrentPage} />
-      {!loading ? <NewsBanner item={news[0]} /> : <Skeleton type="banner" />}
-      {!loading ? <NewsList items={news} /> : <Skeleton count={10} type="item" />}
-      <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={setCurrentPage} />
-    </main>
+    <>
+      <Categories
+        categories={categories}
+        onSelect={setSelectedCategory}
+        selected={selectedCategory}
+      />
+      <main>
+        <div className="container">
+          <Pagination
+            currentPage={currentPage}
+            totalPages={totalPages}
+            onPageChange={setCurrentPage}
+          />
+          {!loading ? <NewsBanner item={news[0]} /> : <Skeleton type="banner" />}
+          {!loading ? <NewsList items={news} /> : <Skeleton count={10} type="item" />}
+          <Pagination
+            currentPage={currentPage}
+            totalPages={totalPages}
+            onPageChange={setCurrentPage}
+          />
+        </div>
+      </main>
+    </>
   )
 }
 
